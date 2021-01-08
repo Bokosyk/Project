edb575a0a5145db2a4359c86ba1730ae89326e8b
Leandro Parrado <lparrado321@gmail.com>
2021-01-08 15:39:07 -0500

    Openweathermap API call works when city and APIkey is defined.

--- assets/script.js
+++ assets/script.js
@@ -7,19 +7,23 @@ $(document).ready(function () {
     var userInput = "";
 
     //Skeleton code for weather API
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKeyOWM;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKeyOWM;
 
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
+        database: "json",
+        success: function(response) {
+            console.log(response);
+        }
     })
-    console.log(queryURL);
+    // console.log(queryURL);
 
     //QueryURL for youtube playlists still needs to be found.
     var queryURL = "https://www.googleapis.com/youtube/v3/playlists" + userInput + "&appid=" + apiKeyYoutube;
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
     })
 
 
