cc5915bc145e999328d8e345be1327b81485d482
Deleon Francis <dxfrancis.coding@gmail.com>
2021-01-12 10:50:52 -0500

    got youTube up and running on weather page.

--- assets/weather.js
+++ assets/weather.js
@@ -12,14 +12,14 @@ $(document).ready(function () {
       var citySearch = $("#city-input").val();
       // gets the weather data for the user input
       weather(citySearch);
+      runPlaylist2(event.target.value.trim());
     }
   });
 
   // api Keys
   var apiKeyWeather = "&appid=b9a7791a6cf8d58430d53a8881a685bc";
-  var apiKeyYouTube = "AIzaSyCbX4jXaXpEqVNHfPQh_Zn0sktw5SknrB4";
 
-  // function that gets the info from the API
+  // function that gets weather info from the API
   function weather(citySearch) {
     $.ajax({
       method: "GET",
@@ -31,10 +31,10 @@ $(document).ready(function () {
       datatype: "json",
       success: function (response) {
         console.log(response);
-        
+
         var weatherDescription = response.weather[0].description;
 
-        // to temperature, humidity, wind speed on the page
+        // Temperature, description, humidity, wind speed on the page
         $("#cityName").text(response.name + " (" + currentDate + ")");
         $("#temp").text(response.main.temp);
         $("#description").text(weatherDescription);
@@ -43,6 +43,44 @@ $(document).ready(function () {
 
         console.log(weatherDescription);
       },
+    });
+  }
+
+  var apiKeyYouTube = "AIzaSyBuv7KGQPgsCS4Imq98FKEm_d5e5FKtOFo";
+
+  // function that fetches data from api
+  var apiRequest2 = function (url) {
+    return $.ajax({
+      url: url,
+      method: "GET",
+      database: "json",
+    });
+  };
+  function runPlaylist2(weatherDescription) {
+    var musicSearchInput = weatherDescription + " music";
+
+    // Url for YouTube search api
+    var searchUrl2 = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=playlist&maxResults=5&q=${musicSearchInput}&key=${apiKeyYouTube}`;
+
+    apiRequest2(searchUrl2).done(function (response2) {
+      // logic to select a playlistId/channelId/videoId
+      if (response2.items.length === 0) {
+        // we can't find your mood at this time.
+      } else {
+        //   pick a random item number from the items array object
+        var randomIndex2 = Math.floor(
+          Math.random() * Math.floor(response2.items.length - 1)
+        );
+        var playlistId = response2.items[randomIndex2].id.playlistId;
+        var playlistUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKeyYouTube}`;
+        apiRequest2(playlistUrl).done(function (playlist) {
+          console.log(playlist);
+          var publicPlaylistUrl = `https://www.youtube.com/embed?listType=playlist&list=${playlistId}`;
+          $("#yt-player-weather").attr("src", publicPlaylistUrl);
+        });
+      }
+      // after selecting, search for item to play
+      console.log(response2);
     });
   }
 });
